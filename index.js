fc05b43047fcfd847f7bf7b081dda2b7936e3c4d
Eric-Gona <eric.gona254@gmail.com>
2021-11-14 21:46:12 +0300

    declsres a function akanname

---
+++ index.js
@@ -0,0 +1,6 @@
+function akaname(event) {
+
+    var maleName = ["kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame"];
+    var femaleName = ["Akosua", "Adwao", "Abenaa", "Akua ", "Yaa", "Afua", "Ama"];
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursady", "Friday", "Saturday"];
+    event.preventDefault();
