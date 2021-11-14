8603db001b382fedee772ed24b037309c7f96eb9
Eric-Gona <eric.gona254@gmail.com>
2021-11-14 21:51:16 +0300

    declaring variables

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function akaname(event) {
     var femaleName = ["Akosua", "Adwao", "Abenaa", "Akua ", "Yaa", "Afua", "Ama"];
     var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursady", "Friday", "Saturday"];
     event.preventDefault();
+
+    var day = parseInt(document.getElementById("day").value)
+    var month = parseInt(document.getElementById("month").value)
+    var year = parseInt(document.getElementById("year").value);
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    var rname = document.getElementById("aka-name");
+
